4d8add4445d3e3aef4dc32b49fd8bc78825d70d2
Sam Fertig <sfertig007@gmail.com>
2026-08-01 13:42:01 -0400

    added btn rects to shop class

--- src/objects/dayManager.py
+++ src/objects/dayManager.py
@@ -34,16 +34,20 @@ class House:
         self.screen = self.subscreen.screen
         self.workbench_rect = pygame.Rect(304, 35, 43, 11)
         self.show_workbench_btn = False
+        self.shop = Shop(screen)
         self.cam: Vector2D = cam
 
     def update(self, dt, events, player):
         r = self.subscreen.get_global_rect(self.workbench_rect.copy())
         if r.colliderect(player.rect()):
             self.show_workbench_btn = True
-            self.cam.y = -100
+            self.cam.y = -115
         else:
             self.show_workbench_btn = False
             self.cam.y = 0
+
+        if self.show_workbench_btn:
+            self.shop.update(dt, events, player)
         
 
     def render(self, screen, cam: Vector2D):
@@ -51,7 +55,29 @@ class House:
         #render
         self.screen.blit(self.image, (0, 0))
         if self.show_workbench_btn:
-            pass #display show
+            self.shop.render(screen, cam)
+        #update
+        self.subscreen.render(cam)
+
+class Shop:
+    def __init__(self, screen):
+        self.image = Assets.get_image("shop")
+        self.subscreen = SubScreen(-368, -105, 368, 100, "black", screen)
+        self.screen = self.subscreen.screen
+        self.seeds_rect = pygame.Rect(16, 16, 32, 16)
+        self.hoe_rect = pygame.Rect(16, 38, 32, 16)
+
+    def update(self, dt, events, player):
+        pass
+
+    def render(self, screen, cam: Vector2D):
+        self.subscreen.clear()
+        #render
+        self.screen.blit(self.image, (0, 0))
+
+        #debug
+        pygame.draw.rect(self.screen, (255, 0, 0), self.seeds_rect, 1)
+        pygame.draw.rect(self.screen, (255, 0, 0), self.hoe_rect, 1)
         #update
         self.subscreen.render(cam)
 
